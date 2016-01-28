c92348bf7a5e1b223f6ecdb2692b999183e91d3f
DKRRK <riisras@gmail.com>
2016-01-28 10:01:33 +0100

    Experimenting with GUI2

--- Reader.py
+++ Reader.py
@@ -24,37 +24,117 @@
 # !/usr/bin/python
 # !/usr/bin/python
 import sqlite3
+import tkFont
+from Tkinter import *
 
 from Methods import *
+# from tkMessageBox import *
+from tkFileDialog import askdirectory
+import tkTable
 
-path = r"D:\Google Drive\Python\Samples"
+
+class GUI(object):
+    """  def __init__(self, master):
+          self.master=master
+          b = Button(text="Browse...",command=lambda : master.callback())
+          b.pack()
+
+          pad=3
+          self._geom='300x300+0+0'
+          master.geometry("{0}x{1}+0+0".format(
+              master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+          master.bind('<Escape>',self.toggle_geom)
+
+      def toggle_geom(self,event):
+          geom=self.master.winfo_geometry()
+          print(geom,self._geom)
+          self.master.geometry(self._geom)
+          self._geom=geom
+      def callback(self, master):
+          dirname = askdirectory(parent=root,initialdir="/",title='Path to exported files')
+
+      """
+
+    def __init__(self, master):
+        self.master = master
+        fontHead = tkFont.Font(family="Arial", size=10, weight=tkFont.BOLD)
+        fontBold = tkFont.Font(family="Arial", size=8, weight=tkFont.BOLD)
+        fontReg = tkFont.Font(family="Arial", size=8)
+        frameN = Frame(master)
+        frameN.grid(row=0, padx=5, pady=5)
+        frameXBH = Frame(frameN)
+        frameXBH.grid(row=0, columnspan=5, padx=5)
+        Canvas(frameXBH, borderwidth=0, relief="flat", height=1, width=20, background="#cccccc").grid(row=0)
+        Label(frameXBH, text="Forensics Reader", font=fontBold, width=15).grid(row=0, column=1)
+        Canvas(frameXBH, borderwidth=0, relief="flat", height=1, width=800, background="#cccccc").grid(row=0, column=2,
+                                                                                                       sticky="WE")
+        Label(frameN, text="Directory containing exported files:", font=fontReg).grid(row=1, sticky="W")
+        xbPath = Entry(frameN, width=30, font=fontReg)
+        xbPath.grid(row=1, column=1, sticky="W")
+        xbBrowse = Button(frameN, text="Browse for folder", font=fontReg, command=lambda: self.get_dir(xbPath))
+        xbBrowse.grid(row=1, column=2, sticky="W")
+        xbRel = Checkbutton(frameN, text="Save case for later", font=fontReg)
+        xbRel.grid(row=1, column=4, sticky="W")
+        Canvas(frameN, borderwidth=1, relief="groove", width=800, height=0).grid(row=2, columnspan=5, pady=10)
+        # SAVE AND CANCEL
+        btnSave = Button(frameN, text="Start", width=10, command=lambda: self.grid())
+        btnSave.grid(row=3, column=3, sticky="E")
+        btnCancel = Button(frameN, text="Cancel", width=10, command=lambda: self.cancel_btn())
+        btnCancel.grid(row=3, column=4, sticky="W")
+
+    def cancel_btn(self):
+        sys.exit(-1)
+
+    def get_dir(self, master):
+        dirname = askdirectory(mustexist=1, title="Please select folder containing exported files")
+        dirname = dirname.replace("/", "\\")
+        master.delete(0, END)
+        master.insert(0, dirname)
+        self.StartExam(dirname)
+
+    def StartExam(self, dirname):
+        Fetch_Info(Userdb, dirname, UserCursor, "NTUSER.DAT", "UsersInfo",
+                   r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths", "Typed Urls")  # Typed Paths
+        Userdb.commit()
+        ReadSingleReg(OSdb, "SYSTEM", dirname, "Select", "Current", cursorOS, "OsInfo",
+                      "CurrentControlSet")  # CurrentControlSet
+
+    def grid(self):
+        table = tkTable.Table(root, rows=2, cols=10)
+        table.pack()
+        table.pack()
+
+
 OSdb = sqlite3.connect(":memory:")
 Userdb = sqlite3.connect(":memory:")
 UserCursor = Userdb.cursor()
 cursorOS = OSdb.cursor()
-cursorOS.execute('''CREATE TABLE OsInfo(id INTEGER PRIMARY KEY, Name TEXT, Value TEXT,Category TEXT)''')
-UserCursor.execute('''CREATE TABLE UsersInfo(id INTEGER PRIMARY KEY, name TEXT, Value TEXT, Category TEXT)''')
+cursorOS.execute('''CREATE TABLE OsInfo(Id INTEGER PRIMARY KEY, Name TEXT, Value TEXT,Category TEXT)''')
+UserCursor.execute('''CREATE TABLE UsersInfo(Id INTEGER PRIMARY KEY, Name TEXT, Value TEXT, Category TEXT)''')
+
+"""
+# print "Mounted Devices:"
+    # result2 = ReadAllReg(dirname + r"\SYSTEM", "MountedDevices", db)
+    # result3 = ReadAllReg(dirname + r"\SOFTWARE", "Microsoft\\Windows NT\\CurrentVersion", db)
+    # print "Current controlset: %s" % forensicating.control_set_check(path + r'\SYSTEM')
+    # print os_settings(path + r'\SYSTEM', dirname + r'\SOFTWARE')
+    # printList(result1)
+    # printList(result3)
+"""
+
+
+
 
 
 def main():
-    Fetch_Info(Userdb, path, UserCursor, "NTUSER.DAT", "UsersInfo",
-               r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths", "Typed Urls")  # Typed Paths
-    Userdb.commit()
-    ReadSingleReg(OSdb, "SYSTEM", path, "Select", "Current", cursorOS, "OsInfo",
-                  "CurrentControlSet")  # CurrentControlSet
-    OSdb.commit()
 
+    OSdb.commit()
     OSdb.close()
     Userdb.close()
 
-if __name__ == '__main__':
-    main()
 
-
-    # print "Mounted Devices:"
-    # result2 = ReadAllReg(path + r"\SYSTEM", "MountedDevices", db)
-    # result3 = ReadAllReg(path + r"\SOFTWARE", "Microsoft\\Windows NT\\CurrentVersion", db)
-    # print "Current controlset: %s" % forensicating.control_set_check(path + r'\SYSTEM')
-    # print os_settings(path + r'\SYSTEM', path + r'\SOFTWARE')
-    # printList(result1)
-    # printList(result3)
+if __name__ == "__main__":
+    root = Tk()
+    root.resizable(0, 0)
+    app = GUI(root)
+    root.mainloop()

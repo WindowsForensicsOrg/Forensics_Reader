8508567ddb9ad8724ff63e6e4b9f6cd1180ac4b9
rasriis <riisras@gmail.com>
2016-02-09 09:57:45 +0100

    Testing Mounted Devices

--- Reader.py
+++ Reader.py
@@ -32,7 +32,6 @@ from Methods import *
 
 
 class GUI(object):
-
     def __init__(self, master):
         self.master = master
         fontHead = tkFont.Font(family="Arial", size=10, weight=tkFont.BOLD)
@@ -59,8 +58,6 @@ class GUI(object):
         btnCancel = tk.Button(frameN, text="Cancel", width=10, command=lambda: self.cancel_btn())
         btnCancel.grid(row=3, column=4, sticky="W")
 
-
-
     def cancel_btn(self):
         raise SystemExit
 
@@ -68,42 +65,38 @@ class GUI(object):
         xbPath.delete(0, "end")
         xbPath.insert(1, askdirectory(mustexist=1, title="Please select folder containing exported files").replace("/", "\\"))
 
-    def OnDoubleClick(self, event):
-        item = self.tree.selection()[0]
-        if self.tree.item(item,"text") == "User activities":
-            self.tree["displaycolumns"]=("Keyname", "Keyvalue")
+    def OnClick(self, event):
 
-        elif self.tree.item(item,"text") == "Operating System information":
+        item = self.tree.selection()[0]
+        if self.tree.item(item,"text") in ("User activities", "Operating System information", "Mounted Devices"):
             self.tree["displaycolumns"]=("Keyname", "Keyvalue")
+        else:
+            print "Error. Value not in list"
 
 
     def StartExam(self):  # Order:(db, cursor, hive, TableName, regPath, Key, Category):
         ReadAllReg(db, cursor, xbPath.get() + "\\NTUSER.DAT", "Info", r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\TypedPaths", "Typed Urls")  # Typed Paths
+        ReadAllReg(db, cursor, xbPath.get() + "\SYSTEM", "Info", "MountedDevices", "OS") #Mounted devices
         ReadSingleReg(db, cursor, xbPath.get() + "\\SYSTEM", "Info", "Select", "Current", "OS")  # CurrentControlSet
-
-
 
     def _grid(self, master):
         self.StartExam()
         self.create_window(master)
 
-
-
     def create_window(self, master):
         root = tk.Toplevel()
         self.tree = ttk.Treeview(root)
         root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
-
         self.tree["columns"] = ("Keyname", "Keyvalue")
         #tree.column("one", width=100)
-        self.tree.column("Keyname", width=100)
-        self.tree.column("Keyvalue", width=2000)
+        self.tree.column("#0", width=300) #First column
+        self.tree.column("Keyname",width=100)
+        self.tree.column("Keyvalue", width=2000 )
         #tree.heading("one", text="ID")
-        self.tree.heading("Keyname", text="Key Name")
-        self.tree.heading("Keyvalue", text="Key value")
+        self.tree.heading("Keyname", text="Key Name",anchor=tk.W)
+        self.tree.heading("Keyvalue", text="Key value",anchor=tk.W)
         self.tree.insert("", 3, "dirOS", open=False, text="Operating System information")
         self.tree.insert("", 3, "dirUser",open=False, text="User activities")
-
         cursor.execute('''SELECT * FROM %s''' % "Info")
         all_rows = cursor.fetchall()
         fo = open("Info.txt", "wb")
@@ -119,28 +112,24 @@ class GUI(object):
                     self.tree.insert("dirURLS", 3, text="", values=(row[1], row[2]))
                 except:
                     self.tree.insert("dirURLS", 3, text="", values=(row[1], row[2]))
-
         #tree.insert("", 0, text="Line 1", values=("1A", "1b"))
         #id2 = tree.insert("", 1, "dir2", text="Dir 2")
         #tree.insert(id2, "end", "dir 2", text="sub dir 2", values=("2A", "2B"))
         ###alternatively:
-
         #tree.insert("dir3", 3, text=" sub dir 3", values=("3A", " 3B"))
       #  tree.insert("", 3, "dir4", text="Dir 4")
       #  tree.insert("dir4", 3, "dir5", text="subdir")
       #  tree.insert("dir5", 3, text=" sub dir 5", values=("3A", " 3B"))
         #tree.insert("dir4", 3, text=" sub dir 4", values=("33", " 333"))
         #tree.pack()
-        self.tree.bind("<<TreeviewOpen>>", self.OnDoubleClick)
+        self.tree.bind("<<TreeviewOpen>>", self.OnClick)
         self.tree.pack(expand=1, fill='both', side='bottom')
         fo.close()
         root.mainloop()
 
-
 db = sqlite3.connect(":memory:")
 cursor = db.cursor()
 cursor.execute('''CREATE TABLE Info(Id INTEGER PRIMARY KEY, Name TEXT, Value TEXT,Category TEXT)''')
-
 
 """
 # print "Mounted Devices:"
@@ -153,8 +142,6 @@ cursor.execute('''CREATE TABLE Info(Id INTEGER PRIMARY KEY, Name TEXT, Value TEX
 def main():
     db.commit()
     db.close()
-
-
 
 if __name__ == "__main__":
     root = tk.Tk()

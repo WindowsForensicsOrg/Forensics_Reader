ce0d7b5cc2a76577c69ffe595e2499840fdca780
rasriis <riisras@gmail.com>
2016-04-10 22:14:46 +0200

    Trying smt

--- MRUListExSort.py
+++ MRUListExSort.py
@@ -5,7 +5,7 @@ def MRULIstExSort(list1, value, successfull):
        
         hex_chars = map(int, map(ord, (value.value())))  # Read mrulistex
         i = 0
-        for i, val in enumerate(hex_chars):  # remove '0'
+        for i, val in enumerate(hex_chars):  # remove '0' 
             if val == 255:
                 break
             if i == 0 or i % 4 == 0:  # The numbers are index 0 and every fourth thereafter
